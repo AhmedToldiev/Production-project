1cdf9f6afd554721ed07bc4954d3a372e258957c
AhmedToldiev <toldiev.009@mail.ru>
2025-03-06 14:36:52 +0300

    Добавил роуты начальные и ленивую загрузку

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from './components/Counter';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { Link } from 'react-router-dom';
+import { Suspense } from 'react';
+import { AboutPageAsync } from './pages/AboutPage/Aboutpage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 export default function App() {
 	return (
 		<div className='app'>
-			App
-			<Counter />,
+			<Link to={'/'}>Главная</Link>
+			<Link to={'/about'}>О себе</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path='/' element={<MainPageAsync />}></Route>
+					<Route path='/about' element={<AboutPageAsync />}></Route>
+				</Routes>
+			</Suspense>
 		</div>
 	);
 }
