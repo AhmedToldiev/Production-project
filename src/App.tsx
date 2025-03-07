92c7b3212108c950f3e4e7e1d2ca4d287d5f66bb
AhmedToldiev <toldiev.009@mail.ru>
2025-03-07 23:39:03 +0300

    Тренировочный коммит

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
 
 	return (
 		<div className={`app ${theme}`}>
-			<button onClick={toggleTheme}>Сменить тему</button>
+			<button onClick={toggleTheme}>Поменять тему</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О себе</Link>
 			<Suspense fallback={<div>Loading...</div>}>
