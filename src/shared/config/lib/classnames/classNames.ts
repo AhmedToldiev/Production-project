3f380aea4c9bad7e1daa5785c305d7d2852ab5a8
AhmedToldiev <toldiev.009@mail.ru>
2025-04-06 22:55:17 +0300

    добавил jest тесты в проект

--- src/shared/config/lib/classnames/classNames.ts
+++ src/shared/config/lib/classnames/classNames.ts
@@ -1,17 +1,12 @@
-import React from 'react';
+type Mods = Record<string, boolean | string>
 
-type Mods = Record<string, boolean | string>;
-
-export default function classNames(
-    cls: string,
-    mods: Mods = {},
-    additional: string[] = [],
-): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
         ...additional.filter(Boolean),
-        Object.entries(mods)
-            .filter(([classNames, value]) => Boolean(value))
+        ...Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
             .map(([className]) => className),
-    ].join(' ');
+    ]
+        .join(' ');
 }
