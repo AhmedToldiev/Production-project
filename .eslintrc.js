5a12abd676c02b9d2b31e4a0cb000b487c6ba723
AhmedToldiev <toldiev.009@mail.ru>
2025-04-19 00:12:48 +0300

    подключил storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,7 @@ module.exports = {
         es2021: true,
         jest: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'plugin:i18next/recommended',
-        'eslint-config-prettier',
-    ],
+    extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'eslint-config-prettier', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
@@ -41,7 +36,7 @@ module.exports = {
         'no-tabs': 'off',
         'i18next/no-literal-string': [
             'error',
-            { markupOnly: true, ignoreAttribute: ['data-testid'] },
+            { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] },
         ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
