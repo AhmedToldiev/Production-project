a66d8b5b302558564b236ee8616d74de37182152
AhmedToldiev <toldiev.009@mail.ru>
2025-05-12 01:17:18 +0300

    добавил модалку и порталы

---
+++ src/widgets/Modal/Modal.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/decorators/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Modal } from './Modal';
+
+export default {
+    title: 'widgets/Modal',
+    component: Modal,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = args => <Modal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    isOpen: true,
+    children: 'Черновой текст для модального окна.  Черновой текст для модального окна.',
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+    isOpen: true,
+    children: 'Черновой текст для модального окна.  Черновой текст для модального окна.',
+};
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,9 @@
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+/* eslint-disable i18next/no-literal-string */
 import { classNames } from 'shared/lib/classnames/classNames';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'widgets/Modal/Modal';
+import { Button, ButtonTheme } from 'widgets/Button/Button';
+import { useCallback, useState } from 'react';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,12 +11,24 @@ interface NavbarProps {
 }
 export function Navbar({ className }: NavbarProps) {
     const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
 
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal(prev => !prev);
+    }, []);
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                Черновой текст для модального окна.  Черновой текст для модального окна.
+            </Modal>
+
         </div>
     );
 }
