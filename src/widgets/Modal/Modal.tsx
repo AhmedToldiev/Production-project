1daedc4f3e9b6ae342ddb548ee1cd56c4405d53b
AhmedToldiev <toldiev.009@mail.ru>
2025-10-28 11:09:59 +0300

    подправил стили для модалки

--- src/widgets/Modal/Modal.tsx
+++ src/widgets/Modal/Modal.tsx
@@ -9,18 +9,18 @@ import cls from './Modal.module.scss';
 interface ModalProps {
     className?: string;
     children?: ReactNode;
-    isOpen?:boolean;
-    onClose?:() => void;
+    isOpen?: boolean;
+    onClose?: () => void;
 }
 
 const ANIMATION_DELAY = 300;
-export const Modal = (props : ModalProps) => {
+export const Modal = (props: ModalProps) => {
     const {
         className, children, isOpen, onClose,
     } = props;
 
     const [isClosing, setIsClosing] = useState(false);
-    const timeRef = useRef<ReturnType <typeof setTimeout>>();
+    const timeRef = useRef<ReturnType<typeof setTimeout>>();
     const { theme } = useTheme();
 
     const closeHandler = useCallback(() => {
@@ -32,11 +32,11 @@ export const Modal = (props : ModalProps) => {
             }, ANIMATION_DELAY);
         }
     }, [onClose]);
-    const contentClick = (e:React.MouseEvent) => {
+    const contentClick = (e: React.MouseEvent) => {
         e.stopPropagation();
     };
 
-    const onKeyDown = useCallback((e:KeyboardEvent) => {
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
         if (e.key === 'Escape') {
             closeHandler();
         }
@@ -51,14 +51,13 @@ export const Modal = (props : ModalProps) => {
         };
     }, [isOpen, onKeyDown]);
 
-    const mods:Record<string, boolean> = {
+    const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
     };
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div className={cls.content} onClick={contentClick}>
                         {children}
